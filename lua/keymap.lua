41b427de9000ab6bb94042203f8a61355281c680
libert <tenliber@gmail.com>
2025-08-13 10:53:10 +0530

    move line up/down

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,3 +4,12 @@ vim.keymap.set("n", "<leader>cf", ":!clang-format -i %<CR>", { noremap = true, s
 
 -- Format selected code
 vim.keymap.set("v", "<leader>cf", ":!clang-format<CR>", { noremap = true, silent = true })
+ 
+
+
+-- for moving line 
+vim.keymap.set("n", "<leader>j" , ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k" , ":m .-2<CR>==", { noremap = true, silent = true })
+
+vim.keymap.set("v", "<leader>k", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>j", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
--- lua/plugin/which-key.lua
+++ lua/plugin/which-key.lua
@@ -6,7 +6,11 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		triggers = {
+			{ "z=", mode = { "n", "v" } },
+		},
 	},
+
 	keys = {
 		{
 			"<leader>?",
