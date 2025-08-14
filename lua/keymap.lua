a66654df95c51f8a223f45b143dcc2036140f370
libert <tenliber@gmail.com>
2025-08-14 18:05:29 +0530

    updated plugins

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,3 +13,6 @@ vim.keymap.set("n", "<leader>k" , ":m .-2<CR>==", { noremap = true, silent = tru
 
 vim.keymap.set("v", "<leader>k", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<leader>j", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+
+
