f64d080951355fcd60fdae327f3d621b0e957f64
libert <tenlibert@gmail.com>
2025-12-29 15:01:51 +0530

    added features

--- lua/keymaps/keymap.lua
+++ lua/keymaps/keymap.lua
@@ -13,19 +13,12 @@ vim.o.breakat = " \t;:,!?"
 -- disable the status bar of nvim  | if you don't use tmux you can comment the below line.
 -- vim.o.laststatus = 0
 
--- keymap config
-local wk = require("which-key")
-wk.add({
-	{
-		{ "<leader>j", ":m .+1<CR>==", mode = "n" },
-		{ "<leader>k", ":m .-2<CR>==", mode = "n" },
-		{ "<leader>k", ":m '<-2<CR>gv", mode = "v" },
-		{ "<leader>j", ":m '>+1<CR>gv", mode = "v" },
-	},
-	{
-		{ "<CR>", "o<Esc>", mode = "n" },
-	},
-	{
-		{ "<C-l>", ":noh<CR>", mode = "n" },
-	},
-}, { noremap = true, silent = true })
+key = vim.keymap.set
+opts = { noremap = true, silent = true }
+
+key("n", "<leader>j", ":m .+1<CR>==", opts)
+key("n", "<leader>k", ":m .-2<CR>==", opts)
+key("x", "<leader>k", ":m '<-2<CR>gv", opts)
+key("x", "<leader>j", ":m '>+1<CR>gv", opts)
+key("n", "<CR>", "o<Esc>", opts)
+key("n", "<C-l>", ":noh<CR>", opts)
