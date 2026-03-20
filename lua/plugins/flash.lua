40da1abb2606f9967b42f77e5a69349123c0a9c6
libert <tenlibert@gmail.com>
2026-03-20 22:20:32 +0530

    files

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,12 +2,13 @@ return {
 
 	{
 		"folke/flash.nvim",
-		event = "verylazy",
+		event = "VeryLazy",
 		opts = {},
 		keys = {
-			{ "ss",    mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "flash" },
-			{ "r",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "treesitter search" },
-			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "toggle flash search" },
+			{ "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",  mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash TS" },
+			{ "r",  mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",  mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "TS Search" }
 		},
-	},
+	}
 }
